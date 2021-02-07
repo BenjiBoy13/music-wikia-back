2a75dd3157c6ab62d8001bbf8ce1617a66311218
Benjamin Gil Flores <benjamin.gil@milenio.com>
2021-02-06 18:21:40 -0600

    First Commit

---
+++ src/main/java/com/lucidprogrammers/projects/musicwikiaback/MusicWikiaBackApplication.java
@@ -0,0 +1,13 @@
+package com.lucidprogrammers.projects.musicwikiaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicWikiaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicWikiaBackApplication.class, args);
+    }
+
+}
